675a78ef18d0a4a2a579aaca78eedb2e51eb9852
listviann <mixail.larionov.2002@mail.ru>
2024-07-04 01:49:19 +0500

    test: add new validation and result unit test

---
+++ StringExpressionCalculator.Tests/ExpressionCalculatorTests.cs
@@ -0,0 +1,47 @@
+﻿namespace StringExpressionCalculator.Tests;
+
+public class ExpressionCalculatorTests
+{
+    [Test]
+    public void Calculate_EmptyStringAsParam_ReturnsZero()
+    {
+        Assert.That(ExpressionCalculator.Calculate(""), Is.EqualTo(0));
+    }
+
+    [Test]
+    public void Calculate_StringWithSingleNumberAsParam_ReturnsTheNumber()
+    {
+        Assert.That(ExpressionCalculator.Calculate("1"), Is.EqualTo(1));
+    }
+
+    [TestCase("1+2-3", true)]
+    [TestCase("1+(2-3)", true)]
+    [TestCase("1+(2-3)", true)]
+    [TestCase("2*(5+5*2)/3+(6/2+8)", true)]
+    [TestCase("(1+(4+5+2)-3)+(6+8)", true)]
+    [TestCase("(1+(4+5+2)-3)+(6.1+8)", true)]
+    public void ValidateBraces_StringWithCorrectBracesOrder_ReturnsTrue(string expression, bool result)
+    {
+        Assert.That(ExpressionCalculator.ValidateBraces(expression), Is.EqualTo(result));
+    }
+
+    [TestCase("1+(2-3", false)]
+    [TestCase("1+2-3)", false)]
+    [TestCase("2*((5+5*2)/3+(6/(2+8)", false)]
+    [TestCase(")))))(1+(4+5+2)-3)+(6.1+8)(((", false)]
+    public void ValidateBraces_StringWithIncorrectBracesOrder_ReturnsFalse(string expression, bool result)
+    {
+        Assert.That(ExpressionCalculator.ValidateBraces(expression), Is.EqualTo(result));
+    }
+    
+    [TestCase("1+2-3", 0)]
+    [TestCase("2*(5+5*2)/3+(6/2+8)", 21)]
+    [TestCase("(1+(4+5+2)-3)+(6.1+8)", 23.1)]
+    [TestCase("3+2*2", 7)]
+    [TestCase("3/2", 1.5)]
+    [TestCase("3+5/2", 5)]
+    public void Calculate_StringExpression_ReturnsResult(string expression, decimal result)
+    {
+        Assert.That(ExpressionCalculator.Calculate(expression), Is.EqualTo(result));
+    }
+}
---
+++ StringExpressionCalculator/ExpressionCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+namespace StringExpressionCalculator;
+
+public class ExpressionCalculator
+{
+    public static bool ValidateBraces(string expression)
+    {
+        string braces = Regex.Replace(expression, @"[^\(\)]", "");
+
+        var stack = new Stack<char>();
+
+        for (int i = 0; i < braces.Length; i++)
+        {
+            if (braces[i] == '(')
+                stack.Push(braces[i]);
+
+            else if (stack.Count > 0 && braces[i] == ')' && stack.Peek() == '(')
+                stack.Pop();
+
+            else
+                return false;
+        }
+
+        if (stack.Count == 0)
+            return true;
+
+        return false;
+    }
+
+    public static decimal Calculate(string expression)
+    {
+        if (string.IsNullOrWhiteSpace(expression))
+        {
+            return 0;
+        }
+
+        return Convert.ToDecimal(expression);
+    }
+}
